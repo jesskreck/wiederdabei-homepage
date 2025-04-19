5e64984f9e5f2b6478754e85225e4cee6b8cb570
jesskreck <krecker.jessica@gmail.com>
2025-04-19 19:54:25 +0200

    Hotjar error fixed

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,6 @@
 <script lang="ts">
 	import '../app.css';
-	import Hotjar from '@hotjar/browser';
-
-	const siteId = 6377167;
-	const hotjarVersion = 6;
-
-	Hotjar.init(siteId, hotjarVersion);
-
+	
 	let { children } = $props();
 </script>
 
